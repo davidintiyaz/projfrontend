e50293f7b13bde07a6f5ca97698c704e6c2dded7
davidintiyaz <davidmalan9898@gmail.com>
2023-02-22 22:37:00 +0530

    updated

--- src/Routes.js
+++ src/Routes.js
@@ -7,6 +7,9 @@ import AdminRoute from "./auth/helper/AdminRoutes";
 import PrivateRoute from "./auth/helper/PrivateRoutes";
 import UserDashBoard from "./user/UserDashBoard";
 import AdminDashBoard from "./user/AdminDashBoard";
+import AddCategory from "./admin/AddCategory";
+import ManageCategories from "./admin/ManageCategories";
+import AddProduct from "./admin/AddProduct";
 
 const Routes = () => {
   return (
@@ -17,6 +20,17 @@ const Routes = () => {
         <Route path="/signin" exact component={Signin} />
         <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
         <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
+        <AdminRoute
+          path="/admin/create/category"
+          exact
+          component={AddCategory}
+        />
+        <AdminRoute
+          path="/admin/categories"
+          exact
+          component={ManageCategories}
+        />
+        <AdminRoute path="/admin/create/product" exact component={AddProduct} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/admin/ManageCategories.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Base from "../core/Base";
+
+const ManageCategories = () => {
+  return (
+    <Base
+    title = "Manage Category Here"
+    description = "Manage Category">
+      <h1 className="text-white">Your assignment</h1>
+    </Base>
+  );
+};
+
+export default ManageCategories;
